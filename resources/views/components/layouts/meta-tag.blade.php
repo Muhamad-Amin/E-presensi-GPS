1b6ed76ea264ecccb6978309ae6762f98bed94eb
Muhamad Amin <terbarubug7@gmail.com>
2025-11-24 18:26:00 +0800

    Set up tampilan dashboard

---
+++ resources/views/components/layouts/meta-tag.blade.php
@@ -0,0 +1,11 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+<meta http-equiv="Pragma" content="no-cache" />
+<meta http-equiv="Expires" content="0" />
+<meta name="viewport"
+    content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover" />
+<meta name="apple-mobile-web-app-capable" content="yes" />
+<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+<meta name="theme-color" content="#000000">
+<meta name="description" content="E-Presensi Geolocation UI Kit">
+<meta name="keywords" content="bootstrap 4, mobile template, cordova, phonegap, mobile, html" />
